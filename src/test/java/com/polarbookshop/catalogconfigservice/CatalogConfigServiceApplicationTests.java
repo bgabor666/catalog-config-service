6334772a8d6110b1fe090de84ee8ac76cfcc5c0f
Gabor Ballabas <gbr666@gmail.com>
2024-10-19 16:03:16 +0200

    Setup repo

---
+++ src/test/java/com/polarbookshop/catalogconfigservice/CatalogConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
